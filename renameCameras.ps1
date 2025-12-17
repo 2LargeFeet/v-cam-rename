42cdebec1e8f51f10db5510858060930e0cd200b
2LargeFeet <michael.gombos@gmail.com>
2025-12-17 13:31:43 -0500

    typo fix

--- renameCameras.ps1
+++ renameCameras.ps1
@@ -2,7 +2,7 @@
 # You can find the org id in Admin > API & Integrations
 # The user you use will need site admin rights in the site with the cameras
 #
-# This script relies ont the verkada powershell module. Install it with 'Install-Module -Name verkadaModule'
+# This script relies on the verkada powershell module. Install it with 'Install-Module -Name verkadaModule'
 #
 # Once this command is run you can then run the renameCameras.ps1 script with the following command.
 # ./renameCameras.ps1 -org_id "<Your Verkada Org ID>"
